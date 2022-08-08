2e8a349c8fba73c6b8516680be4a6cc54569b31b
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-08 15:31:47 +0200

    initial app

---
+++ frontend/ui/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      wertey
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  // components: {
+  //   HelloWorld,
+  // },
+})
+export default class HomeView extends Vue {}
+</script>
