4db461ba0ec6bff71fd10d86f43000e8fef6eec1
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-08 16:38:20 +0200

    added backend app

---
+++ frontend/ui/src/components/Uploads/UploadFile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-input">
+    <input type="file" label="file" hidden id="fileUpload" aria-label="file">
+    <button @click="uploadFile">
+      Upload File
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  // props: {
+  //   msg: String,
+  // },
+})
+export default class UploadFile extends Vue {
+  // msg!: string
+
+  uploadFile() {
+    const fileUpload = document.getElementById('fileUpload');
+    if (fileUpload) {
+      fileUpload.click();
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .app-input {
+    color: red;
+  }
+</style>
--- frontend/ui/src/views/HomeView.vue
+++ frontend/ui/src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-      wertey
-  </div>
+  <upload-file />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import UploadFile from '@/components/Uploads/UploadFile.vue';
 
 @Options({
+  components: {
+    UploadFile,
+  },
+
   // components: {
   //   HelloWorld,
   // },
